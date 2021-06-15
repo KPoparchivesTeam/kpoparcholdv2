30dee6153fd06283f5832b99d0ffb1b32aef50a1
fazemodz <hike820@gmail.com>
2021-06-15 07:10:44 +0100

    initial commit

---
+++ templates/navbartemplate.php
@@ -0,0 +1,12 @@
+
+<div class="topnav">
+<a href="index">Home</a>
+<a href="Blackpink">BlackPink</a>
+<a href="RedVelvet">RedVelvet</a>
+<a href="Loona">Loona</a>
+<a href="Twice">Twice</a>
+<a href="Everglow">Everglow</a>
+<a href="Ateez">Ateez</a>
+<a href="Acknowledgements">Acknowledgements</a>
+<a href="K-Dramas/">K-Dramas</a>
+</div>
